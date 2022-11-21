34d6935b7b739e187f9a6594eb35f5f5afebc926
Dodominionek <dt532@interia.pl>
2022-11-21 21:20:18 +0100

    Working

--- prediction.py
+++ prediction.py
@@ -1,6 +1,6 @@
 data_sequence = open("data01.txt", "r").read()
 print(data_sequence)
-window_size = 10
+sequence_size = 20
 probality_of_one = 0.5
 sequence_predictions = {}
 sequence_probability = {}
@@ -8,61 +8,96 @@ sequence_occurences = {}
 our_param = 0.1
 hit = 0
 
-if window_size > len(data_sequence):
+
+accuracies = []
+
+def updateProb(occ, prob):
+    updatedProb = 0
+    for o in range(1, occ + 1):
+        updatedProb += pow(prob, o)
+    # print(updatedProb)
+    return updatedProb
+
+
+
+if sequence_size > len(data_sequence):
     print("Window is to big")
 
 i = 0
 
-for ch in data_sequence[window_size:len(data_sequence):1]:
-    print(ch, "\n")
-    if data_sequence[0 + i:window_size + i] not in sequence_predictions:
-        print("ins")
+for ch in data_sequence[sequence_size:len(data_sequence):1]:
+    if data_sequence[0 + i:sequence_size + i] not in sequence_predictions:
         if probality_of_one >= 0.5:
-            if ch == 1:
-                sequence_predictions[str(data_sequence[0 + i:window_size + i])] = 1
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one + our_param
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = 1
+            if ch == '1':
+                sequence_predictions[str(data_sequence[0 + i:sequence_size + i])] = 1
+                sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one + updateProb(1, our_param)
+                sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = 1
                 hit += 1
             else:
-                sequence_predictions[str(data_sequence[0 + i:window_size + i])] = 0
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one - our_param
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = 1
+                sequence_predictions[str(data_sequence[0 + i:sequence_size + i])] = 0
+                sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one - updateProb(1, our_param)
+                sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = 1
         else:
-            if ch == 0:
-                sequence_predictions[str(data_sequence[0 + i:window_size + i])] = 0
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one - our_param
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = 1
+            if ch == '0':
+                sequence_predictions[str(data_sequence[0 + i:sequence_size + i])] = 0
+                sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one - updateProb(1, our_param)
+                sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = 1
                 hit += 1
             else:
-                sequence_predictions[str(data_sequence[0 + i:window_size + i])] = 1
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one + our_param
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = 1
-            
+                sequence_predictions[str(data_sequence[0 + i:sequence_size + i])] = 1
+                sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one + updateProb(1, our_param)
+                sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = 1
+
     else:
-        if sequence_probability[str(data_sequence[0 + i:window_size + i])] >= 0.5:
-            if ch == sequence_predictions[str(data_sequence[0 + i:window_size + i])] and ch == 1:
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one + pow(our_param, sequence_occurences[str(data_sequence[0 + i:window_size + i])])
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = sequence_occurences[str(data_sequence[0 + i:window_size + i])] + 1
-                hit += 1
-            elif ch == sequence_predictions[str(data_sequence[0 + i:window_size + i])] and ch == 0:
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one - pow(our_param, sequence_occurences[str(data_sequence[0 + i:window_size + i])])
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = sequence_occurences[str(data_sequence[0 + i:window_size + i])] - 1
+        if sequence_probability[str(data_sequence[0 + i:sequence_size + i])] >= 0.5:
+            if ch == sequence_predictions[str(data_sequence[0 + i:sequence_size + i])]:
+                if ch == '1':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one + updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] + 1
+                    hit += 1
+                elif ch == '0':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one - updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] - 1
+                    hit += 1
+            else:
+                if ch == '1':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one - updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] - 1
+                elif ch == '0':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one + updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] + 1
         else:
-            if ch == sequence_predictions[str(data_sequence[0 + i:window_size + i])] and ch == 0:
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one - pow(our_param, sequence_occurences[str(data_sequence[0 + i:window_size + i])])
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = sequence_occurences[str(data_sequence[0 + i:window_size + i])] + 1
-                hit += 1
-            elif ch == sequence_predictions[str(data_sequence[0 + i:window_size + i])] and ch == 1:
-                sequence_probability[str(data_sequence[0 + i:window_size + i])] = probality_of_one + pow(our_param, sequence_occurences[str(data_sequence[0 + i:window_size + i])])
-                sequence_occurences[str(data_sequence[0 + i:window_size + i])] = sequence_occurences[str(data_sequence[0 + i:window_size + i])] - 1
+            if ch == sequence_predictions[str(data_sequence[0 + i:sequence_size + i])]:
+                if ch == '1':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one - updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] - 1
+                    hit += 1
+                elif ch == '0':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one + updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] + 1
+                    hit += 1
+            else:
+                if ch == '1':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one + updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] + 1
+                elif ch == '0':
+                    sequence_probability[str(data_sequence[0 + i:sequence_size + i])] = probality_of_one - updateProb(sequence_occurences[str(data_sequence[0 + i:sequence_size + i])], our_param)
+                    sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] = sequence_occurences[str(data_sequence[0 + i:sequence_size + i])] - 1
+
+    if i == 300 or i == 500 or i == 1000:
+
+        accuracies.append(float(hit/i))
+        print(F"NUMER I: {str(i)} blad: {accuracies} = {hit} / {i}")
+        pass
+
     i += 1
 
-print("\n\n")
-print(sequence_predictions)
-print("\n\n")
-print(sequence_probability)
-print("\n\n")
-print(sequence_occurences)
-print("\n\n")
+# print("\n\n")
+# print(sequence_predictions)
+# print("\n\n")
+# print(sequence_probability)
+# print("\n\n")
+# print(sequence_occurences)
+# print("\n\n")
 
 print(hit / i)
